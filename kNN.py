38bd2cb6eb82fc62a781a517c60ef3cc1b06b0ca
Moran96 <37325670+Moran96@users.noreply.github.com>
2018-03-26 11:09:59 +0800

    Update kNN.py

--- kNN.py
+++ kNN.py
@@ -7,7 +7,7 @@ def createDataSet():
 	labels = ['A','A','B','B']
 	return group,labels
 
-#--------------------------------------------------------
+#----------------------------------------------------------------
 #There are 4 input parameters:
 #input vector X, training instance set, attribute vector and num of neighbors.
 
@@ -26,7 +26,7 @@ def classify0(inX,dataSet,labels,k):
 
 	return sortedClassCount[0][0]
 
-#--------------------------------------------------------
+#----------------------------------------------------------------
 #This function is to solve the problems of data format.
 #Input the filename in str.
 #Output the matrix of training instance and class_label_vector.
@@ -51,3 +51,44 @@ def file_to_matrix(filename):
 	print(lis_contain_float[-1],type(lis_contain_float[-1]))
 	fr.close()
 	return mat_return,class_label_vector
+
+#----------------------------------------------------------------
+#This function can change the data from (min,max) to (0,1).
+#The new value of each data obey the rules:
+#        new_value = (old_value - min)/(max-min)
+#Input dataset;
+#Output new dataset, ranges and min_values
+
+def auto_norm(data_set):
+	min_values = data_set.min(0)
+	max_values = data_set.max(0)
+	ranges = max_values - min_values
+	norm_data_set = zeros(shape(data_set))
+	m = data_set.shape[0]
+	norm_data_set = data_set - tile(min_values,(m,1))
+	norm_data_set = norm_data_set/tile(ranges,(m,1))
+
+	return norm_data_set,ranges,min_values
+
+#----------------------------------------------------------------
+#
+#
+#Test
+
+def dating_class_test():
+	ho_ratio = 0.10
+	dating_data_mat,dating_labels = file_to_matrix("datingTestSet.txt")
+	norm_mat,ranges,min_values = auto_norm(dating_data_mat)
+	m = norm_mat.shape[0]
+	num_test_vecs = int(m*ho_ratio)
+	error_count = 0.0
+
+	for i in range(num_test_vecs):
+		classifier_result = classify0(norm_mat[i,:],norm_mat[num_test_vecs:m,:],dating_labels[num_test_vecs:m],3)
+		print("the classifier came back with: %d, the real answer is: %d" %(classifier_result,dating_labels[i]))
+
+		if (classifier_result != dating_labels[i]):
+			error_count +=1.0
+	print("the total error rate is: %f" %(error_count/float(num_test_vecs)))
+
+#-----------------------------------------------------------------------------
